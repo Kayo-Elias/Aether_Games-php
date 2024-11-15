871bbddc3e8692f422d3cdcc2d185de65749789d
Kayo-Elias <eliaskayo579@gmail.com>
2024-11-15 16:13:15 -0300

    Estrutura

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="pt-BR">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Aether Games</title>
+        <Link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+        <div class="topo-texto">
+          <p>Bem-vindo ao Aether Games! confira as últimas novidades do mundo dos games.</p>
+
+        </div>
+
+
+</body>
+
+
+</html>
