099f552fd26dcca5e5a480206663db28cb571437
joaopedro115 <jpedroherculano06@gmail.com>
2024-11-19 21:51:59 -0300

    update index

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html lang="pt-BR">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Aether Games</title>
+        <Link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+
+            <!-- cabeçalho -->
+         <header>
+            <div class="logo-container">
+                
+            <img src="img/Aether.png" alt="Logo do site" class="logo">
+             <h1>Aether Games</h1>
+            </div>
+             <nav>
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="noticias.php">Noticias</a></li>
+                    <li><a href="reviews.php">Reviews</a></li>
+                </ul>
+             </nav>
+         </header>
+         
+                  <!-- principal -->
+
+                  <main>
+                    <h2>Últimas notícias</h2>
+                    <p>Bem-vindo a nossa página inicial. Aqui você encontrará as notícias e reviews sobre os últimos lançamentos do mundo dos games.</p>
+
+                </main>
+
+                 <!-- rodapé -->
+                  <footer>
+                    <p>&copy; 2024 Aether Games</p>
+                  </footer>
+
+
+
+
+</body>
+</html>
