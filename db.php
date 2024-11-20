306e7165539c42a856fafe90d5a40cfbbf33f0b3
Kayo-Elias <eliaskayo579@gmail.com>
2024-11-20 14:18:08 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,13 @@
 <?php
 $host = 'localhost';
 $dbname = 'aether_games';
-$username = 'root';
-$password = '';
+$username = 'root';    //usuario
+$password = '';        //senha
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro ao conectar ao banco de dados:" . $e->getMessage());
+}
+?>
