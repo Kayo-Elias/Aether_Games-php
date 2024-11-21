5448e98eee1c74e4c4606db0286f4cd5a4f8ea89
joaopedro115 <jpedroherculano06@gmail.com>
2024-11-20 22:05:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,22 @@
             <img src="img/Aether.png" alt="Logo do site" class="logo">
              <h1>Aether Games</h1>
             </div>
+
+            <div class="redes-sociais-container">
+            <a href="https://twitter.com/aethergames" target="_blank">
+            <img src="img/twitter-icon.png" alt="Twitter" class="rede-social-icon">
+</a>
+            <a href="https://facebook.com/aethergames" target="_blank">
+            <img src="img/facebook-icon.png" alt="Facebook" class="rede-social-icon">
+</a> 
+            <a href="https://instagram.com/aethergames" target="_blank">
+            <img src="img/instagram-icon.png" alt="Instagram" class="rede-social-icon">
+</a>
+            <a href="https://youtube.com/aethergames" target="_blank">
+                    <img src="img/youtube-icon.png" alt="YouTube" class="rede-social-icon">
+</a>
+            </div>
+            </div>
              <nav>
                 <ul>
                     <li><a href="index.php">Inicio</a></li>
@@ -41,16 +57,6 @@
                     <p>Bem-vindo a nossa página inicial. Aqui você encontrará as notícias e reviews sobre os últimos lançamentos do mundo dos games.</p>
 
                      
-
-                    <!--seção de redes sociais-->
-                    <section class="redes sociais">
-                    <h2>Siga-nos nas redes sociais</h2>
-                    <a href="https://twitter.com/aethergames" target="_blank">Twitter</a>
-                    <a href="https://facebook.com/aethergames" target="_blank">Facebook</a>
-                    <a href="https://instagram.com/aethergames" target="_blank">Instagram</a>
-                    <a href="https://youtube.com/aethergames" target="_blank">YouTube</a>
-                   
-                   </section>
 
 
                 </main>
