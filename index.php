b6bbcc36b4aa016e2c35ad98fe1a2a416e828a2b
joaopedro115 <jpedroherculano06@gmail.com>
2024-11-20 22:15:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -21,16 +21,16 @@
 
             <div class="redes-sociais-container">
             <a href="https://twitter.com/aethergames" target="_blank">
-            <img src="img/twitter-icon.png" alt="Twitter" class="rede-social-icon">
+            <img src="https://toppng.com/public/uploads/preview/twitter-x-new-logo-round-icon-png-11692480241tdbz6jparr.webp" alt="Twitter" class="rede-social-icon">
 </a>
             <a href="https://facebook.com/aethergames" target="_blank">
-            <img src="img/facebook-icon.png" alt="Facebook" class="rede-social-icon">
+            <img src="https://th.bing.com/th/id/R.2bad70f2d08429a28dfbebd4c237924b?rik=vgEdhJ%2f%2biiEnQQ&riu=http%3a%2f%2fpngimg.com%2fuploads%2ffacebook_logos%2ffacebook_logos_PNG19748.png&ehk=0ZiZ04ZZ6mSJ5oyPxBh1gy4FSYhegWTWyDpCiI73sbw%3d&risl=&pid=ImgRaw&r=0" alt="Facebook" class="rede-social-icon">
 </a> 
             <a href="https://instagram.com/aethergames" target="_blank">
-            <img src="img/instagram-icon.png" alt="Instagram" class="rede-social-icon">
+            <img src="https://th.bing.com/th/id/R.735dda68880a385ce8cc5be4f3c5fcd6?rik=qSxRw2lCZYy9Mw&riu=http%3a%2f%2fpngimg.com%2fuploads%2finstagram%2finstagram_PNG11.png&ehk=QVCbfkCKi8pJLF08bRkS%2fLeMqLTnJQf402WRaIdN6jE%3d&risl=&pid=ImgRaw&r=0" alt="Instagram" class="rede-social-icon">
 </a>
             <a href="https://youtube.com/aethergames" target="_blank">
-                    <img src="img/youtube-icon.png" alt="YouTube" class="rede-social-icon">
+            <img src="https://www.liblogo.com/img-logo/yo482y5ff-youtube-icon-logo-youtube-logo-free-icon-of-vector-logo.png" alt="YouTube" class="rede-social-icon">
 </a>
             </div>
             </div>
