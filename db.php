462f0194056241edd55beae8716b76a154ed1669
Kayo-Elias <eliaskayo579@gmail.com>
2024-11-28 19:26:02 -0300

    Update

--- db.php
+++ db.php
@@ -1,13 +1,5 @@
 <?php
-$host = 'kayo';
-$dbname = 'aether_games';
-$username = 'kayo';    //usuario
-$password = 'kayo55811111123';        //senha
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
 $host = 'localhost';      
 $usuario = 'root';        
 $senha = '';              
