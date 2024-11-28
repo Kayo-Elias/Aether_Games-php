ffca1e2eb05ffe4a333dd9e856a70d80c1eab3a2
joaopedro115 <jpedroherculano06@gmail.com>
2024-11-28 17:41:50 -0300

    Update db.php

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';      
+$usuario = 'root';        
+$senha = '';              
+$banco = 'aether_games'; 
+
+// Criar conexão com o banco de dados
+$conexao = new mysqli($host, $usuario, $senha, $banco);
+
+// Verificar se houve erro na conexão
+if ($conexao->connect_error) {
+    die("Erro na conexão com o banco de dados: " . $conexao->connect_error);
+}
+?>
