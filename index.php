d260403ca410a6f7da5d8096ca2ebdafe1073586
joaopedro115 <jpedroherculano06@gmail.com>
2024-11-30 09:58:50 -0300

    update style.css e index.php

--- index.php
+++ index.php
@@ -63,19 +63,20 @@ if (!isset($conexao)) {
         $resultado = $conexao->query($sql);
 
         if ($resultado->num_rows > 0):
-            while ($noticia = $resultado->fetch_assoc()): 
+            while ($noticia = $resultado->fetch_assoc()):
         ?>
             <article>
-                <?php if ($noticia['imagem']): ?>
-                    <img src="img/<?php echo $noticia['imagem']; ?>" alt="Imagem da notícia" class="noticia-img">
+                <h2><?php echo htmlspecialchars($noticia['titulo']); ?></h2>
+                <?php if (!empty($noticia['imagem'])): ?>
+                    <img src="<?php echo htmlspecialchars($noticia['imagem']); ?>" alt="Imagem da notícia" class="noticia-img">
                 <?php endif; ?>
-                <h2><?php echo $noticia['titulo']; ?></h2>
-                <p><?php echo substr(htmlspecialchars($noticia['conteudo']), 0, 150) . '...'; ?></p>
-                <small><?php echo $noticia['data']; ?></small>
-                <a href="noticias.php?id=<?php echo $noticia['id']; ?>" class="ver-mais">Leia mais</a>
+                <p><?php echo nl2br(htmlspecialchars(substr($noticia['conteudo'], 0, 150))); ?>...</p>
+                <a href="noticia_completa.php?id=<?php echo $noticia['id']; ?>" class="ver-mais">Leia mais</a>
             </article>
-        <?php endwhile; ?>
-        <?php else: ?>
+        <?php
+            endwhile;
+        else:
+        ?>
             <p>Nenhuma notícia encontrada.</p>
         <?php endif; ?>
         </div>
@@ -84,21 +85,21 @@ if (!isset($conexao)) {
     <!-- Rodapé -->
     <footer>
         <div class="redes-sociais-container">
-            <h2>Siga-nos nas redes sociais</h2>
-            <a href="https://facebook.com/aethergames" class="rede-social-link">
-                <img src="https://th.bing.com/th/id/R.2bad70f2d08429a28dfbebd4c237924b?rik=vgEdhJ%2f%2biiEnQQ&riu=http%3a%2f%2fpngimg.com%2fuploads%2ffacebook_logos%2ffacebook_logos_PNG19748.png&ehk=0ZiZ04ZZ6mSJ5oyPxBh1gy4FSYhegWTWyDpCiI73sbw%3d&risl=&pid=ImgRaw&r=0" alt="Facebook" class="rede-social-icon">
-            </a>
-            <a href="https://x.com/aethergames_?t=OkI8IvGnlaga-8LS0QC99Q&s=09" class="rede-social-link">
+            <h2>Siga-nos:</h2>
+            <a href="https://x.com/aethergames_?t=OkI8IvGnlaga-8LS0QC99Q&s=09" target="_blank">
                 <img src="https://www.freepnglogos.com/uploads/twitter-x-logo-png/twitter-x-logo-png-9.png" alt="Twitter" class="rede-social-icon">
             </a>
-            <a href="https://www.instagram.com/aethergames1?igsh=dGRhN3k4NWltNzV5" class="rede-social-link">
-                <img src="https://th.bing.com/th/id/R.b473fcad595f09b0afe1270d9016ac8c?rik=UkXeB5L2aE8Biw&riu=http%3a%2f%2fpngimg.com%2fuploads%2finstagram%2finstagram_PNG9.png&ehk=GnhAoqBr5eV3LRtLGDB2XElOW7mvkdk29%2byVkqj09y8%3d&risl=&pid=ImgRaw&r=0" alt="Instagram" class="rede-social-icon">
+            <a href="https://facebook.com/aethergames" target="_blank">
+                <img src="https://th.bing.com/th/id/R.2bad70f2d08429a28dfbebd4c237924b?rik=vgEdhJ%2f%2biiEnQQ&riu=http%3a%2f%2fpngimg.com%2fuploads%2ffacebook_logos%2ffacebook_logos_PNG19748.png&ehk=0ZiZ04ZZ6mSJ5oyPxBh1gy4FSYhegWTWyDpCiI73sbw%3d&risl=&pid=ImgRaw&r=0" alt="Facebook" class="rede-social-icon">
             </a>
-            <a href="https://www.youtube.com/channel/UCZD3jFO-zq_RkJHTnBVOSIg"  class="rede-social-link">
+            <a href="https://www.instagram.com/aethergames1?igsh=dGRhN3k4NWltNzV5" target="_blank">
+                <img src="https://th.bing.com/th/id/R.735dda68880a385ce8cc5be4f3c5fcd6?rik=qSxRw2lCZYy9Mw&riu=http%3a%2f%2fpngimg.com%2fuploads%2finstagram%2finstagram_PNG11.png&ehk=QVCbfkCKi8pJLF08bRkS%2fLeMqLTnJQf402WRaIdN6jE%3d&risl=&pid=ImgRaw&r=0" alt="Instagram" class="rede-social-icon">
+            </a>
+            <a href="https://www.youtube.com/channel/UCZD3jFO-zq_RkJHTnBVOSIg" target="_blank">
                 <img src="https://logodownload.org/wp-content/uploads/2014/10/youtube-logo-5-2.png" alt="YouTube" class="rede-social-icon">
             </a>
         </div>
-        <p>&copy; 2024 Aether Games. Todos os direitos reservados.</p>
+        <p>&copy; 2024 Aether Games</p>
     </footer>
 
 </body>
